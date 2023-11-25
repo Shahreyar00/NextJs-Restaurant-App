1a99ad4d18dd28bd0b40e63bd7caf7a5ccc6e61f
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2023-11-25 22:30:50 +0530

    created first phase of UI components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import Menu from './Menu';
+
+const Navbar = () => {
+    const user = false;
+
+    return (
+        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase lg:px-20 xl:px-40">
+            <div className="hidden md:flex gap-4 flex-1">
+                <Link href="/">Home</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/">Contact</Link>
+            </div>
+            <div className="text-2xl font-bold flex-1 text-center">
+                <Link href="/">Pizzeria</Link>
+            </div>
+            <div className="md:hidden">
+                <Menu />
+            </div>
+            <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+                <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+                    <Image src="/phone.png" alt="" width={20} height={20} />
+                    <span>12345 67890</span>
+                </div>
+                {!user ? (
+                    <Link href="/login">Login</Link>
+                ) : (
+                    <Link href="/orders">Orders</Link>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
